19f05c0760239e7faf3c1f36064f2bd1721f3482
Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
2024-04-20 02:43:35 -0700

    made script print lcoation info

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("travelForm").addEventListener("submit", function(event) {
     event.preventDefault();
     const destination = document.getElementById("destination").value;
-    fetch('https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US', {
+    fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US`, {
         "method": "GET",
         "headers": {
             "X-RapidAPI-key": "f87fe54ac5msh3f62659484924a8p14a7e4jsnb2cc27d65afb",
@@ -10,8 +10,9 @@ document.getElementById("travelForm").addEventListener("submit", function(event)
     })
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         const locationId = data.data[0].result_object.location_id;
-        return fetch(`https://api.travel-advisor.p.rapidapi.com/attractions/list?location_id=${locationId}&currency=USD&lang=en_US&lunit=mi&sort=reccomended`, {
+        return fetch(`https://travel-advisor.p.rapidapi.com/attractions/list?location_id=${locationId}&currency=USD&lang=en_US&lunit=mi&sort=recommended`, {
             "method": "GET",
             "headers": {
                 "X-RapidAPI-key": "f87fe54ac5msh3f62659484924a8p14a7e4jsnb2cc27d65afb",
@@ -21,6 +22,7 @@ document.getElementById("travelForm").addEventListener("submit", function(event)
     })
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         const attractions = data.data;
         const itineraryContent = document.getElementById("itineraryContent");
         itineraryContent.innerHTML = "";
